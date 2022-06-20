a9c6b3cdb3b87fb03267e2824e4156d373304404
Julien <julienmulle@gmail.com>
2022-06-20 16:20:01 +0200

    maj 2

--- src/app/mock-developper.ts
+++ src/app/mock-developper.ts
@@ -4,5 +4,5 @@ import { skillList } from "./mock-skills";
 
 export const DEVELOPPERLIST: Developper[] = [
     new Developper("Dupont", "Jean", 25, "masculin", "Je suis un développeur", skillList),
-    new Developper("Dupont", "Jean", 25, "masculin", "Je suis un développeur", skillList),
+    new Developper("L'eponge", "Bob", 25, "masculin", "Je suis un développeur", skillList),
 ];
--- src/app/mock-skills.ts
+++ src/app/mock-skills.ts
@@ -3,9 +3,5 @@ export const skillList: skill[] = [
     (new skill("Angular", "angular.png", "https://angular.io/")),
     (new skill("TypeScript", "typescript.png", "https://www.typescriptlang.org/")),
     (new skill("HTML", "html.png", "https://www.w3schools.com/html/")),
-    (new skill("CSS", "css.png", "https://www.w3schools.com/css/")),
-    (new skill("JavaScript", "javascript.png", "https://www.w3schools.com/js/")),
-    (new skill("NodeJS", "nodejs.png", "https://nodejs.org/en/")),
-    (new skill("PHP", "php.png", "https://www.php.net/")),
-    (new skill("SQL", "sql.png", "https://www.w3schools.com/sql/")),
+    (new skill("CSS", "css.png", "https://www.w3schools.com/css/"))
 ];
